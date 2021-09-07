95588e9e52b4288e804fbe8af6253ad63a0226df
parksangwoo <b01056721617@gmail.com>
2021-09-07 23:14:25 +0900

    ⚡ :: schoolService

--- src/main/java/com/rest/practice/service/SchoolService.java
+++ src/main/java/com/rest/practice/service/SchoolService.java
@@ -1,12 +1,15 @@
 package com.rest.practice.service;
 
+import com.rest.practice.dto.SchoolResponse;
 import com.rest.practice.model.School;
 import com.rest.practice.model.Student;
 import com.rest.practice.repository.SchoolRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,14 +17,22 @@ public class SchoolService {
 
     private final SchoolRepository schoolRepository;
 
+    @Transactional
     public void establishSchool(School school) {
         schoolRepository.save(school);
     }
 
-    public List<School> schoolList() {
-        return schoolRepository.findAll();
+    @Transactional
+    public List<SchoolResponse> schoolList() {
+        return schoolRepository.findAll()
+                .stream().map(school -> SchoolResponse.builder()
+                        .createdDate(school.getCreatedDate())
+                        .schoolName(school.getSchoolName())
+                        .build())
+                .collect(Collectors.toList());
     }
 
+    @Transactional
     public School findSchool(String schoolName) {
         return schoolRepository.findSchoolBySchoolName(schoolName);
     };
