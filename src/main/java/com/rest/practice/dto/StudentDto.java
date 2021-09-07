6402543cc017ceff1c5c6423f10ce3008ddfd8bd
parksangwoo <b01056721617@gmail.com>
2021-09-07 22:47:43 +0900

    ⚡ :: StudentDTO

--- src/main/java/com/rest/practice/dto/StudentDto.java
+++ src/main/java/com/rest/practice/dto/StudentDto.java
@@ -1,13 +1,17 @@
 package com.rest.practice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class StudentDto {
 
     private String name;
     private String school;
-
+    private LocalDateTime createdDate;
 }
