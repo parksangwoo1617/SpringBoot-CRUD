ed557db64602c99332f73d26a67d9c20e146c900
parksangwoo <b01056721617@gmail.com>
2021-09-08 21:38:14 +0900

    ⚡ :: 어노테이션 주석처리

--- src/test/java/com/rest/practice/PracticeApplicationTests.java
+++ src/test/java/com/rest/practice/PracticeApplicationTests.java
@@ -3,7 +3,7 @@ package com.rest.practice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PracticeApplicationTests {
 
 	@Test
