8c86a0cec47d0f5cdb6eeb936f5b589003ebf8a7
parksangwoo <b01056721617@gmail.com>
2021-09-13 09:22:00 +0900

    ⚡ :: SchoolRepository

--- src/main/java/com/rest/practice/repository/SchoolRepository.java
+++ src/main/java/com/rest/practice/repository/SchoolRepository.java
@@ -1,10 +1,7 @@
 package com.rest.practice.repository;
 
 import com.rest.practice.model.School;
-import com.rest.practice.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface SchoolRepository extends JpaRepository<School, Long> {
 
